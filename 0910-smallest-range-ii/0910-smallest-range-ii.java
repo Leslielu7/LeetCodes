b68657f7ddcf1aa0b899cbc980da83a85c14a794
Leslielu7 <60060239+Leslielu7@users.noreply.github.com>
2022-10-23 23:21:38 -0400

    Time: 43 ms (5.03%), Space: 50.1 MB (8.17%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.java
+++ 0910-smallest-range-ii/0910-smallest-range-ii.java
@@ -7,6 +7,8 @@ class Solution {
         Arrays.sort(nums);
         int result = nums[nums.length-1] - nums[0];
         
+        
+        
         for(int i = 0; i < nums.length-1; i++){
             int high = Math.max(nums[nums.length-1] - k, nums[i] + k);
             int low = Math.min(nums[0] + k, nums[i+1] - k);
